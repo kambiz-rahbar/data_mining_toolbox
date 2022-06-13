c07579c0a934cec6a3269aa842cf2c3c87670e39
ANCHA <45118134+kambiz-rahbar@users.noreply.github.com>
2022-06-14 01:00:31 +0430

    Add files via upload

--- cosin_similarity.m
+++ cosin_similarity.m
@@ -16,7 +16,7 @@ y(rand(size(y)) < 0.1) = nan; % add miss data
 %% calculate dissimilarity
 match_threshold = 0.1;
 
-dissimilarity = ones(size(x, 1), size(y, 1));
+cos_similarity = ones(size(x, 1), size(y, 1));
 for i = 1 : size(x, 1)
     for j = 1 : size(y, 1)
 
@@ -29,16 +29,14 @@ for i = 1 : size(x, 1)
         % remove miss features
         xf(miss_data_index) = [];
         yf(miss_data_index) = [];
-
-        compare_data = abs(xf - yf) < match_threshold;
-        dissimilarity(i,j) = (numel(compare_data) - sum(compare_data)) / numel(compare_data);
-        %similarity(i,j) = sum(compare_data) / numel(compare_data);
+        
+        cos_similarity(i, j) = sum(xf .* yf) / (sqrt(sum(xf .^ 2)) * sqrt(sum(yf .^ 2)));
     end
 end
 
-similarity = 1 - dissimilarity;
+cos_dissimilarity = 1 - cos_similarity;
 
 %% show results
-subplot(1,2,1); imagesc(similarity); title('similarity'); colorbar; axis square;
-subplot(1,2,2); imagesc(dissimilarity); title('dissimilarity'); colorbar; axis square;
+subplot(1,2,1); imagesc(cos_similarity); title('cos similarity'); colorbar; axis square;
+subplot(1,2,2); imagesc(cos_dissimilarity); title('cos dissimilarity'); colorbar; axis square;
 colormap gray
--- dissimilarity_of_binaries_data.m
+++ dissimilarity_of_binaries_data.m
@@ -2,14 +2,20 @@ clc
 clear
 close all
 
+%% build sample boolean data
 number_of_records_in_x = 10;
 number_of_data_in_eaxh_record_for_x = 20;
 x = rand(number_of_records_in_x, number_of_data_in_eaxh_record_for_x) > 0.5;
+x = uint8(x);
+x(rand(size(x)) < 0.1) = nan; % add miss data
 
 number_of_records_in_y = 15;
 number_of_data_in_eaxh_record_for_y = 20;
 y = rand(number_of_records_in_y, number_of_data_in_eaxh_record_for_y) > 0.5;
+y = uint8(y);
+y(rand(size(y)) < 0.1) = nan; % add miss data
 
+%% check if symetric
 symetric_threshold = 0.05;
 
 if (sum(x(:))-sum(1-x(:)))/numel(x) < symetric_threshold && (sum(y(:))-sum(1-y(:)))/numel(y) < symetric_threshold
@@ -26,15 +32,27 @@ else
     jaccard = ones(size(x, 1), size(y, 1));
 end
 
+%% calculate dissimilarity
 match_threshold = 0.1;
 
 dissimilarity = ones(size(x, 1), size(y, 1));
 for i = 1 : size(x, 1)
     for j = 1 : size(y, 1)
-        q = sum(x(i, :) == y(j, :) & x(i, :) == 1);
-        t = sum(x(i, :) == y(j, :) & x(i, :) == 0);
-        r = sum(x(i, :) ~= y(j, :) & x(i, :) == 1);
-        s = sum(x(i, :) ~= y(j, :) & x(i, :) == 0);
+
+        xf = x(i, :);
+        yf = y(j, :); 
+
+        % to not consider miss data
+        miss_data_index = isnan(x(i,:)) | isnan(y(j,:));
+        
+        % remove miss features
+        xf(miss_data_index) = [];
+        yf(miss_data_index) = [];
+
+        q = sum(xf == yf & xf == 1);
+        t = sum(xf == yf & xf == 0);
+        r = sum(xf ~= yf & xf == 1);
+        s = sum(xf ~= yf & xf == 0);
 
         if is_data_symetric_binary
             dissimilarity(i, j) = (r + s) / (q + t + r + s);
@@ -47,6 +65,7 @@ end
 
 similarity = 1 - dissimilarity;
 
+%% show results
 figure(1);
 colormap gray;
 
