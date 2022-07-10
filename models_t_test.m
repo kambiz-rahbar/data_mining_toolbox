9d9b3ca9bfa31b78bba89a5cb7a3f6b9e2f2f069
ANCHA <45118134+kambiz-rahbar@users.noreply.github.com>
2022-07-10 13:02:32 +0430

    Update models_t_test.m

--- models_t_test.m
+++ models_t_test.m
@@ -23,13 +23,13 @@ variance_model_1_and_model_2 = mean( (model_1_error_for_K_fold_testing - model_2
 t = (average_error_for_model_1 - average_error_for_model_2)...
     /sqrt(variance_model_1_and_model_2 / K);
 
-significant_threshold = 0.5;
-
-z = significant_threshold / 2;
+%significant_threshold = 0.5;
+%z = significant_threshold / 2;
 
 degree_of_freedom = K-1;
+z = tcdf(t, degree_of_freedom);
 
-if abs(t) < tcdf(t, degree_of_freedom)
+if abs(t) < z
     disp('H0 hypothesis rejected: The difference between the models is significant.');
 else
     disp('H0 hypothesis accepted: Both models are the same.');
